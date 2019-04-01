9bb5175ae7b44cd2a601077905d9108d8166d69a
zhaoyunxing92 <2385585770@qq.com>
2019-04-01 19:21:40 +0800

    [init]

---
+++ spring-boot-netty/src/main/java/io/github/xyz/spring/boot/netty/SpringBootNettyApplication.java
@@ -0,0 +1,13 @@
+package io.github.xyz.spring.boot.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNettyApplication.class, args);
+    }
+
+}
