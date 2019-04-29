a418f7916b3e5e562a3b934f0de26a6e1c2e50b2
zhaoyunxing92 <2385585770@qq.com>
2019-04-29 21:06:56 +0800

    [add]aliyun for kafka

---
+++ spring-boot-aliyun-kafka/src/main/java/io/github/xyz/spring/boot/aliyun/kafka/AliyunKafkaServer.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.spring.boot.aliyun.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-04-29 20:56
+ * @des:
+ */
+@SpringBootApplication
+public class AliyunKafkaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunKafkaServer.class, args);
+    }
+}
