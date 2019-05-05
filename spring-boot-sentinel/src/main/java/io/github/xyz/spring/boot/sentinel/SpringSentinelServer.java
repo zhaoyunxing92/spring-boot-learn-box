24dd65a292688493f47e65e8cf71aae6aff840d2
zhaoyunxing92 <2385585770@qq.com>
2019-05-05 11:21:05 +0800

    [add]sentinel jar

---
+++ spring-boot-sentinel/src/main/java/io/github/xyz/spring/boot/sentinel/SpringSentinelServer.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.spring.boot.sentinel;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-05-05 10:32
+ * @des:
+ */
+public class SpringSentinelServer {
+}
