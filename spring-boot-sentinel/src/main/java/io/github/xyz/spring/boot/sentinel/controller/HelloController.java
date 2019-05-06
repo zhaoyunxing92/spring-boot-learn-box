d5483456e3e3502a92f2699185d141a4bfd679ef
zhaoyunxing92 <2385585770@qq.com>
2019-05-06 21:31:03 +0800

    [up]

--- spring-boot-sentinel/src/main/java/io/github/xyz/spring/boot/sentinel/controller/HelloController.java
+++ spring-boot-sentinel/src/main/java/io/github/xyz/spring/boot/sentinel/controller/HelloController.java
@@ -4,14 +4,11 @@
 package io.github.xyz.spring.boot.sentinel.controller;
 
 import io.github.xyz.spring.boot.sentinel.service.HelloService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhaoyunxing
