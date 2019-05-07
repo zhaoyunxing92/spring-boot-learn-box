729b74eda22b4314832541032a20848f1d05d83e
zhaoyunxing92 <2385585770@qq.com>
2019-05-07 19:54:54 +0800

    [rm]移除maven版本

---
+++ spring-boot-apollo/src/main/java/io/github/xyz/spring/boot/apollo/SpringApolloServer.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.spring.boot.apollo;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-05-07 19:52
+ * @des:
+ */
+public class SpringApolloServer {
+}
