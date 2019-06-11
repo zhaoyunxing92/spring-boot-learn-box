0d6da0b70f85b5a573fa45c39d4157c4d15053a5
zhaoyunxing92 <2385585770@qq.com>
2019-06-12 00:59:18 +0800

    [up]修改日志方式

--- spring-boot-netty/src/main/java/io/github/xyz/spring/boot/netty/websocket/WebSocketStart.java
+++ spring-boot-netty/src/main/java/io/github/xyz/spring/boot/netty/websocket/WebSocketStart.java
@@ -7,7 +7,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+    import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
