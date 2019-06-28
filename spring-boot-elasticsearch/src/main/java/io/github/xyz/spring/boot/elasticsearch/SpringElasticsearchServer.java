da3a4f0553bcd05a8cfd844b1074406106a0ef98
zhaoyunxing92 <2385585770@qq.com>
2019-06-28 16:39:40 +0800

    [add]elasticsearch

---
+++ spring-boot-elasticsearch/src/main/java/io/github/xyz/spring/boot/elasticsearch/SpringElasticsearchServer.java
@@ -0,0 +1,17 @@
+package io.github.xyz.spring.boot.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-06-28 14:23
+ * @des:
+ */
+@SpringBootApplication
+public class SpringElasticsearchServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticsearchServer.class, args);
+    }
+}
