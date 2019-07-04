802e2d81a621e9c5531d8de5ff1bae9a37e62b91
zhaoyunxing92 <2385585770@qq.com>
2019-07-04 15:35:49 +0800

    [rm]删除冗余代码

--- spring-boot-elasticsearch/spring-boot-jest-elasticsearch/src/main/java/io/github/xyz/spring/boot/jest/elasticsearch/entity/User.java
+++ spring-boot-elasticsearch/spring-boot-jest-elasticsearch/src/main/java/io/github/xyz/spring/boot/jest/elasticsearch/entity/User.java
@@ -5,8 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
 
 /**
  * @author zhaoyunxing
