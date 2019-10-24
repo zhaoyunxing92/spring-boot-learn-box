2da604d62c99936a8853e91127c134b6689fb4f2
zhaoyunxing92 <2385585770@qq.com>
2019-10-24 08:23:44 +0800

    [up]更新pom

---
+++ spring-boot-rocketmq/dubbo-dtx-bank-01/src/main/java/io/github/xyz/spring/boot/rocketmq/Bank01.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.spring.boot.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-10-24 00:00
+ */
+@SpringBootApplication
+public class Bank01 {
+    public static void main(String[] args) {
+        SpringApplication.run(Bank01.class, args);
+    }
+}
