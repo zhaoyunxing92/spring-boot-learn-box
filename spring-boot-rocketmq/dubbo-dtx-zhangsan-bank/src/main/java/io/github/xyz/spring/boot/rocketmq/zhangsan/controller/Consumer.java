1d47c5182262fcfbb7fdbb6f6575be2b4898984a
zhaoyunxing92 <2385585770@qq.com>
2019-10-28 15:07:24 +0800

    [up]更新配置

---
+++ spring-boot-rocketmq/dubbo-dtx-zhangsan-bank/src/main/java/io/github/xyz/spring/boot/rocketmq/zhangsan/controller/Consumer.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.xyz.spring.boot.rocketmq.zhangsan.controller;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-10-28 14:30
+ */
+public class Consumer {
+}
