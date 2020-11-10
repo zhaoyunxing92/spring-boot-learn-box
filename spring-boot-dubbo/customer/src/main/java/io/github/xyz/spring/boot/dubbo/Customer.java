cc09dd73a3c2a4e3796f09e805bf23f80132ab29
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-11-10 16:53:57 +0800

    fix:修复告警

---
+++ spring-boot-dubbo/customer/src/main/java/io/github/xyz/spring/boot/dubbo/Customer.java
@@ -0,0 +1,6 @@
+package io.github.xyz.spring.boot.dubbo;/**
+ *
+ * @author zhaoyunxing
+ * @date 2020/11/10 14:00
+ */public class Customer {
+}
---
+++ spring-boot-dubbo/provider/src/main/java/io/github/xyz/spring/boot/dubbo/Provider.java
@@ -0,0 +1,6 @@
+package io.github.xyz.spring.boot.dubbo;/**
+ *
+ * @author zhaoyunxing
+ * @date 2020/11/10 16:44
+ */public class Provider {
+}
