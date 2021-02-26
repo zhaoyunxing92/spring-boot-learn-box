d0b303f6632228cd38ddf81be63932968916f36e
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2021-02-26 15:21:36 +0800

    up:更新dubbo用例

--- spring-boot-dubbo/dubbo-customer/dubbo-customer-service/src/main/java/io/github/xyz/spring/boot/dubbo/service/impl/HelloServiceImpl.java
+++ spring-boot-dubbo/dubbo-customer/dubbo-customer-service/src/main/java/io/github/xyz/spring/boot/dubbo/service/impl/HelloServiceImpl.java
@@ -1,6 +1,7 @@
 package io.github.xyz.spring.boot.dubbo.service.impl;
 
 import io.github.xyz.spring.boot.dubbo.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.rpc.RpcContext;
 
@@ -8,12 +9,16 @@ import org.apache.dubbo.rpc.RpcContext;
  * @author zhaoyunxing
  * @date 2020/11/10 19:07
  */
+@Slf4j
 @DubboService
 public class HelloServiceImpl implements HelloService {
 
     @Override
     public String echo() {
-
+        RpcContext context = RpcContext.getContext();
+        String hello = context.getAttachment("hello");
+        log.info("attachment:{}", hello);
+        context.setAttachment("name", "dubbo");
         return "hello " + RpcContext.getContext().getRemoteApplicationName();
     }
 }
--- spring-boot-dubbo/dubbo-provider/src/main/java/io/github/xyz/spring/boot/dubbo/controller/HelloController.java
+++ spring-boot-dubbo/dubbo-provider/src/main/java/io/github/xyz/spring/boot/dubbo/controller/HelloController.java
@@ -1,7 +1,10 @@
 package io.github.xyz.spring.boot.dubbo.controller;
 
 import io.github.xyz.spring.boot.dubbo.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author zhaoyunxing
  * @date 2020/11/10 19:15
  */
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -17,6 +21,13 @@ public class HelloController {
 
     @GetMapping("/echo")
     public String echo() {
-        return helloService.echo();
+        RpcContext context = RpcContext.getContext();
+        context.setAttachment("hello", "provider");
+
+        String echo = helloService.echo();
+        String name = context.getAttachment("name");
+
+        log.info("name:{}", name);
+        return echo;
     }
 }
